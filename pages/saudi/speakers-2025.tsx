605484f56e5e33c9713f666a3df47565a516492a
NaveedHabib <159003808+NaveedHabib@users.noreply.github.com>
2025-08-07 09:07:03 +0500

    changes

--- pages/saudi/speakers-2025.tsx
+++ pages/saudi/speakers-2025.tsx
@@ -38,7 +38,41 @@ export default function SpeakersPage() {
         <h1 className="text-4xl font-bold mb-10 text-center">Speakers 2025</h1>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8">
+          {speakers.map((speaker: conferenceSpeakerModel2025) => (
+            speaker.HighLevel == true &&(
+ <div
+              key={speaker.ItemID}
+              onClick={() => openPopup(speaker)}
+              className="bg-white rounded-lg shadow p-4 flex flex-col items-center text-center hover:shadow-lg transition duration-200"
+            >
+              <div className="w-40 h-40 mb-4">
+                <Image
+                  width={400}
+                  height={400}
+                  src={speaker.Image}
+                  alt={`${speaker.FirstName} ${speaker.LastName}`}
+                  className="w-full h-full object-cover object-top border border-gray-200"
+                  onError={(e) => (e.currentTarget.src = "/default-avatar.jpg")}
+                />
+              </div>
+              <h3 className="text-lg font-semibold">
+                {speaker.FirstName} {speaker.LastName}
+              </h3>
+              <p className="text-sm text-gray-600">{speaker.Designation}</p>
+              <p className="text-sm text-gray-500">{speaker.Company}</p>
+            </div>
+            )
+           
+          ))}
+        </div>
+      </div>
+
+      <div className="max-w-7xl mx-auto px-4">
+        {/* <h1 className="text-4xl font-bold mb-10 text-center">Speakers 2025</h1> */}
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8">
           {speakers.map((speaker) => (
+              speaker.HighLevel == false &&(
             <div
               key={speaker.ItemID}
               onClick={() => openPopup(speaker)}
@@ -60,6 +94,7 @@ export default function SpeakersPage() {
               <p className="text-sm text-gray-600">{speaker.Designation}</p>
               <p className="text-sm text-gray-500">{speaker.Company}</p>
             </div>
+              )
           ))}
         </div>
       </div>
