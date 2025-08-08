e00077a5696d144b6770925240b6ca1b60fe3332
NaveedHabib <159003808+NaveedHabib@users.noreply.github.com>
2025-08-08 15:31:33 +0500

    added google tag

--- components/Layout/LayoutComponent.tsx
+++ components/Layout/LayoutComponent.tsx
@@ -10,6 +10,9 @@ export default function LayoutComponent({ children }: any) {
       {!isRegisterInterestRoute && <MenuComponent />}
 
       <main className="flex-grow">{children}</main>
+
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-THXPZM8Q"
+        height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
     </div>
   );
 }
